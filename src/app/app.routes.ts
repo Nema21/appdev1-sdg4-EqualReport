540fb47b4cbafb609af17a8680a3e1fad15835d3
Harry Bahatan <20238447@s.ubaguio.edu>
2026-04-22 20:38:22 +0800

    Added initial routing for main pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ReportDashboardComponent } from './pages/report-dashboard/report-dashboard.component';
+import { ReportDetailComponent } from './pages/report-detail/report-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  { path: 'dashboard', component: ReportDashboardComponent },
+
+  { path: 'detail/:id', component: ReportDetailComponent },
+
+  { path: 'about', component: AboutComponent },
+
+  { path: '**', component: NotFoundComponent }
+];
