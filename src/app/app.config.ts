fc007aa36fcdd17e52dd9f9c5a9b45be063bdd73
SandrexBiore-02 <20246263@s.ubaguio.edu>
2026-04-23 12:40:54 +0800

    Implement parameterized detail view using API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 providers: [
   provideHttpClient()
--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,18 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-<<<<<<< HEAD
-import { CanActivateFn } from '@angular/router';
-import { authGuard } from './auth.guard';
-
-describe('AuthGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
-=======
 import { AuthGuard } from './auth.guard';
 import { Router } from '@angular/router';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
->>>>>>> 725407012df35b2c982cb644f7656a99d11742e6
 
   beforeEach(() => {
     TestBed.configureTestingModule({
