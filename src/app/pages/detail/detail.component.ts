dfe0280160aee5bc67e1c1c167e9e2f2a0356f50
SandrexBiore-02 <20246263@s.ubaguio.edu>
2026-04-23 12:35:32 +0800

    Implement parameterized detail view using API

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ReportService, Report } from '../../services/report.service';
 
 @Component({
-  selector: 'app-report-detail',
-  imports: [],
-  templateUrl: './detail.component.html',
-  styleUrl: './detail.component.css'
+  selector: 'app-detail',
+  templateUrl: './detail.component.html'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
 
+  report!: Report;
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: ReportService
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.service.getReportById(id).subscribe(data => {
+      this.report = data;
+    });
+  }
 }
