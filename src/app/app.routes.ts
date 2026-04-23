e6831937253b412f6d524fc5ccb30189c0e44a69
SandrexBiore-02 <20246263@s.ubaguio.edu>
2026-04-23 11:43:42 +0800

    Fix routing using existing pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DetailComponent } from './pages/detail/detail.component';
+import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-
-  { path: 'dashboard', component: DashboardComponent },
-
-  { path: 'detail/:id', component: DetailComponent },
-
   { path: 'about', component: AboutComponent },
+// Main Features
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'dashboard/:id', component: DetailComponent, canActivate: [authGuard] },
 
   { path: '**', component: NotFoundComponent }
 ];
