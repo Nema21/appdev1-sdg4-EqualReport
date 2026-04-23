8f8fcd7bd6ad09f9215de30ccffb75a0af714d1e
Harry Bahatan <20238447@s.ubaguio.edu>
2026-04-23 12:20:47 +0800

    added dashboard display and API service for fetching data

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Report {
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
 
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) {}
+
+  getReports(): Observable<Report[]> {
+    return this.http.get<Report[]>(this.apiUrl);
+  }
+
+  getReportById(id: number): Observable<Report> {
+    return this.http.get<Report>(`${this.apiUrl}/${id}`);
+  }
 }
